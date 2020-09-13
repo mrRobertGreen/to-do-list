2be7a2b224b4e33e16344e37dccd26f91e0e8138
Robert Green <mrrobertgreen17@gmail.com>
2020-09-13 14:05:37 +0300

    delete conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC} from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import {Home} from "./pages/Home/Home";
 import {Login} from "./pages/Login/Login";
-import {useDispatch} from "react-redux";
-import {getCards} from "./redux/tasks-reducer/tasks-reducer";
 
 export const App: FC = () => {
-   const dispatch = useDispatch()
-   useEffect( () => {
-      dispatch(getCards())
-   }, [dispatch])
 
    return (
       <div className="App">
